7dc527664dab8454b5552a8dfb50713d589fc7db
JLODI <88990920+JLODI@users.noreply.github.com>
2024-12-16 20:47:10 -0800

    added comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_17_034944) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_17_044608) do
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
