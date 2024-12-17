e00f0890475eb391b03e49fabb324974a1e3e53c
JLODI <88990920+JLODI@users.noreply.github.com>
2024-12-16 19:45:47 -0800

    added validations in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, length: { maximum: 20, message: "Must be no more than 20 characters in length"}
+    validates :body, presence: true, length: { minimum: 5, message: "Must be at least 5 characters in length"}
 end
