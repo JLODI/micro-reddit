f6fe104812ae45cc3a3b63e5ab928d8e50f77060
JLODI <88990920+JLODI@users.noreply.github.com>
2024-12-16 21:14:08 -0800

    added missing association for post and comments in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
     belongs_to :user
+    has_many :comments, dependent: :destroy
     validates :title, presence: true, length: { maximum: 30, message: "Must be no more than 20 characters in length"}
     validates :body, presence: true, length: { minimum: 5, message: "Must be at least 5 characters in length"}
     validates :user_id, presence: true, numericality: { only_integer: true }
