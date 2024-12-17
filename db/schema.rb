ee39bf7ad16481e4c26f6fa0087e17b7ad88ca2c
JLODI <88990920+JLODI@users.noreply.github.com>
2024-12-16 21:00:47 -0800

    added comments assocations

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_17_044608) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_17_045835) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "post_id", null: false
+    t.integer "user_id", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -35,5 +39,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_17_044608) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
