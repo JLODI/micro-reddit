a019ada5cfaafce882127f01cecc3461f513f836
JLODI <88990920+JLODI@users.noreply.github.com>
2024-12-16 21:05:06 -0800

    added validation for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
     belongs_to :post
     belongs_to :user
+    validates :body, presence: true, length: { minimum: 5, message: "must be atleast 5 characters in length"}
 end
