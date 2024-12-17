b083169f991e4f595ebad1312e99aa00b7f8f4ca
JLODI <88990920+JLODI@users.noreply.github.com>
2024-12-16 19:35:02 -0800

    added validations and error messages to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :username, presence: true, length: { maximum: 12 }
+    validates :username, presence: true, length: { maximum: 12, message: "must be no more than 12 characters in length" }
     validates :email, presence: true
-    validates :password, presence: true, length: { minimum: 5 }
+    validates :password, presence: true, length: { minimum: 5, message: "must be atleast 5 characters in length"}
 end
